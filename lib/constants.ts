e04880de13fa9fac51b2973852d70a70c0fed5d9
codesugar <1033021318@qq.com>
2024-07-07 15:37:52 +0800

    修改自己的网站命名

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,10 +21,10 @@ export const RevalidateOneMonth = 3600 * 24 * 30;
 export const PAGE_SIZE = 20;
 export const InfoPageSize = 12;
 
-export const FORM_PLACEHOLDER = 'https://tap4.ai/';
+export const FORM_PLACEHOLDER = 'https://gen24ai.com/';
 
 export const WEBSITE_EXAMPLE =
-  '<a href="https://tap4.ai/" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>';
+  '<a href="https://gen24ai.com/" title="Gen24 AI Tools Directory">Gen24 AI Tools Diresctory</a>';
 
 export const STARTUP_LIST = [
   {
